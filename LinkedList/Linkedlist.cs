d4ecbd1ba4b02f312320162e84e0f7b6d589638e
Ruhma-desai-786 <romakesari2734@gmail.com>
2023-06-07 09:44:16 +0530

    LinkedList

---
+++ LinkedList/Linkedlist.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedList
+{
+    public class Linkedlist
+    {
+        Node head;
+        public void AddNode(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine("{0} Inserted into LinkedList", node.Data);
+        }
+        public void AddReverse(int data)
+        {
+            Node newnode = new Node(data);
+            if (head == null)
+            {
+                head = newnode;
+            }
+            else
+            {
+                Node temp = head;
+                head = newnode;
+                head.Next = temp;
+            }
+            Console.WriteLine("{0} Inseted inti LinkedList", newnode.Data);
+        }
+
+        public void Append(int data)
+        {
+            Node newnode = new Node(data);
+            if (head == null)
+            {
+                head = newnode;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = newnode;
+            }
+            Console.WriteLine("{0} Inserted into LinkedList", newnode.Data);
+
+        }
+
+        public Node Insert(int position, int data)
+        {
+            if (position < 1)
+            {
+                Console.WriteLine("Invalid position");
+            }
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.Next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while (position-- != 0)
+                {
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.Next = this.head.Next;
+                        head.Next = node;
+                        break;
+                    }
+                    head = head.Next;
+                }
+                if (position != 1)
+                {
+                    Console.WriteLine("posiiton is out of range");
+                }
+            }
+            return head;
+        }
+
+        public void Delete()
+        {
+            Node tempnode = head;
+            if (head != null)
+            {
+                tempnode = head;
+                head = head.Next;
+                tempnode = null;
+            }
+        }
+
+        public void DeleteLast()
+        {
+            Node temp = head;
+            Node lastnode = temp.Next;
+            if(head!=null)
+            {
+                temp = temp.Next;
+                temp.Next = null;
+                lastnode = null;
+            }
+        }
+        //}
+
+
+        public void Display()
+        {
+            if(head==null)
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.WriteLine("Linked List is Empty");
+                Console.ResetColor();
+            }
+            else
+            {
+                Node temp= head;
+                while(temp!=null)
+                {
+                    Console.Write(" | "+temp.Data+" | ");
+                    temp=temp.Next;
+                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Linked List is  Not Empty");
+                Console.ResetColor();
+            }
+            
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace LinkedList
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            while (true)
+            {
+                Linkedlist linkedList = new Linkedlist();
+                Console.WriteLine("Enter the Choice\n1)Add Node\n2)Reverse Node\n3)Append\n4)Inser\n5)Delete" +
+                    "\n6)Delete Last");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        
+                        linkedList.AddNode(56);
+                        linkedList.AddNode(30);
+                        linkedList.AddNode(70);
+                        linkedList.Display();
+                        break;
+                    case 2:
+                        linkedList.AddReverse(56);
+                        linkedList.AddReverse(30);
+                        linkedList.AddReverse(70);
+                        linkedList.Display();
+                        break;
+                    case 3:
+                        linkedList.Append(56);
+                        linkedList.Append(30);
+                        linkedList.Append(70);
+                        linkedList.Display();
+                        break;
+                    case 4:
+                        linkedList.AddNode(56);
+                        linkedList.AddNode(70);
+                        linkedList.Display();
+                        linkedList.Insert(2, 30);
+                        linkedList.Display();
+                        break;
+                    case 5:
+                        linkedList.AddNode(56);
+                        linkedList.AddNode(30);
+                        linkedList.AddNode(70);
+                        linkedList.Display();
+                        linkedList.Delete();
+                        linkedList.Display();
+                        break;
+                    case 6:
+                        linkedList.AddNode(56);
+                        linkedList.AddNode(30);
+                        linkedList.AddNode(70);
+                        linkedList.Display();
+                        linkedList.DeleteLast();
+                        linkedList.Display();
+                        break;
+                }
+            }
+        }
+    }
+}
+
